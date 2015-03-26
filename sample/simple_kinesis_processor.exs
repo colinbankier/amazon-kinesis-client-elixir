26c2983af845f1cc027f89373c4211c24b53a919
colinbankier <colinbankier@gmail.com>
2015-03-27 07:52:00 +1000

    Base64 decode in sample.

--- sample/simple_kinesis_processor.exs
+++ sample/simple_kinesis_processor.exs
@@ -2,12 +2,16 @@ defmodule SimpleKinesisClient do
   import Kcl.RecordProcessor
 
   def process_records(records) do
-    Enum.each records, &(process_record(&1))
+    Enum.each records, &(handle_record(&1))
     seq = records |> List.first |> Map.get "sequenceNumber"
     case checkpoint(seq) do
       :ok -> nil
       {:error, _} -> checkpoint(seq)
     end
+  end
+  def handle_record record do
+    Base.decode64!(record["data"])
+    |> process_record
   end
   def process_record data do
     IO.inspect data
