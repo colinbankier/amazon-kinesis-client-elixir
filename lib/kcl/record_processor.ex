ee21b5c25c6e63cd53d431c19bd43b2c9115c38b
colinbankier <colinbankier@gmail.com>
2015-04-02 17:07:36 +1000

    Add checkpoint on error to record processor.

--- lib/kcl/record_processor.ex
+++ lib/kcl/record_processor.ex
@@ -1,63 +1,56 @@
 defmodule Kcl.RecordProcessor do
   alias Kcl.IOProxy
+  use Timex
+  @default_options [
+    sleep_seconds: 5,
+    checkpoint_retries: 5,
+    checkpoint_freq_seconds: 60
+  ]
+
   defmacro __using__(_) do
     quote do
       import unquote(__MODULE__)
+      use Timex
+      require Logger
 
-      @default_options [
-        sleep_seconds: 5,
-        checkpoint_retries: 5,
-        checkpoint_freq_seconds: 60
-      ]
-
-      def initialize, do: initialize []
       def initialize(options) do
-        Dict.merge(@default_options, options)
-        |> ensure_started
-      end
-
-      def ensure_started, do: ensure_started(@default_options)
-      def ensure_started options do
-        alive? = Process.registered
-        |> Enum.member?(__MODULE__)
-
-        if !alive? do
-          Agent.start_link(fn -> options end, name: __MODULE__)
-        end
-      end
-
-      def state do
-        ensure_started
-        Agent.get(__MODULE__, &(&1))
-      end
-
-      def update fun do
-        ensure_started
-        Agent.update(__MODULE__, fun)
+        Kcl.RecordProcessor.initialize options
       end
 
       def process_records(records) do
-        result = Enum.map records, &(handle_record(&1))
-        seq = records |> List.first |> Map.get "sequenceNumber"
-        case checkpoint(seq) do
-          :ok -> nil
-          {:error, _, _} -> checkpoint(seq)
+        force_checkpoint = false
+        try do
+          Enum.map records, &(handle_record(&1))
+        rescue
+          e ->
+            force_checkpoint = true
+            Logger.error "Error while prcoessing records: #{inspect e}"
+        after
+          check_checkpoint force_checkpoint
         end
+      end
+
+      def handle_record record do
+        result = record["data"]
+        |> Base.decode64!
+        |> process_record
+
+        record["sequenceNumber"]
+        |> parse_int
+        |> update_largest_seq
 
         result
       end
 
-      def handle_record record do
-        record["data"]
-        |> Base.decode64!
-        |> process_record
+      def state do
+        Kcl.RecordProcessor.state
       end
 
       def init_processor(_) do
         update fn state ->
           state
           |> Dict.put(:largest_seq, nil)
-          |> Dict.put(:last_checkpoint_time, Timex.Date.epoch(:secs))
+          |> Dict.put(:last_checkpoint_time, Date.epoch(:secs))
         end
       end
 
@@ -67,6 +60,58 @@ defmodule Kcl.RecordProcessor do
     end
   end
 
+  def initialize(options) do
+    Dict.merge(@default_options, options)
+    |> ensure_started
+  end
+
+  def ensure_started, do: ensure_started @default_options
+  def ensure_started options do
+    alive? = Process.registered |> Enum.member?(__MODULE__)
+
+    if !alive? do
+      Agent.start_link(fn -> options end, name: __MODULE__)
+    end
+  end
+
+  def parse_int(val) when is_integer(val), do: val
+  def parse_int(val), do: Integer.parse(val)
+
+  def state do
+    ensure_started
+    Agent.get(__MODULE__, &(&1))
+  end
+
+  def update fun do
+    ensure_started
+    Agent.update(__MODULE__, fun)
+  end
+
+  def update_largest_seq({seq, _}), do: update_largest_seq seq
+  def update_largest_seq seq do
+    if seq > largest_seq do
+      update fn state ->
+        Dict.put state, :largest_seq, seq
+      end
+    end
+  end
+
+  def check_checkpoint force_checkpoint do
+    if (Date.epoch(:secs) - last_checkpoint_time > state[:checkpoint_freq_seconds]) || force_checkpoint do
+      checkpoint state[:largest_seq]
+      update fn state ->
+        Dict.put state, :last_checkpoint_time, Date.epoch(:secs)
+      end
+    end
+  end
+
+  def last_checkpoint_time do
+    Dict.get state, :last_checkpoint_time, 0
+  end
+
+  def largest_seq do
+    Dict.get state, :largest_seq, 0
+  end
 
   def checkpoint seq do
     IOProxy.write_action("checkpoint", %{checkpoint: seq})
--- test/kcl/record_processor_test.exs
+++ test/kcl/record_processor_test.exs
@@ -2,6 +2,7 @@ defmodule Kcl.RecordProcessorTest do
   use ExUnit.Case
   use Timex
   import TestHelper
+  alias Kcl.IOProxy
 
   defmodule MyProcessor do
     use Kcl.RecordProcessor
@@ -10,12 +11,13 @@ defmodule Kcl.RecordProcessorTest do
   end
 
   setup do
-    Kcl.IOProxy.initialize open_io
     :ok
   end
 
   test "process_record is called in client module with decoded data" do
+    IOProxy.initialize open_io
     records = [create_record("Hello world!"), create_record("foobar")]
+
     assert MyProcessor.process_records(records) == ["I got Hello world!", "I got foobar"]
   end
 
@@ -24,8 +26,6 @@ defmodule Kcl.RecordProcessorTest do
   end
 
   test "default config is set on initialize" do
-    MyProcessor.initialize
-
     assert MyProcessor.state[:sleep_seconds] == 5
     assert MyProcessor.state[:checkpoint_retries] == 5
     assert MyProcessor.state[:checkpoint_freq_seconds] == 60
@@ -42,11 +42,28 @@ defmodule Kcl.RecordProcessorTest do
   end
 
   test "checkpointing is reset on init_processor" do
-    MyProcessor.initialize
     MyProcessor.init_processor 1234
 
     assert MyProcessor.state[:largest_seq] == nil
     assert_in_delta MyProcessor.state[:last_checkpoint_time], Date.epoch(:secs), 1
+  end
+
+  test "forces checkpoint with largest_seq on error" do
+    io = open_io
+    IOProxy.initialize io
+    defmodule BrokenProcessor do
+      use Kcl.RecordProcessor
+        
+      def process_record data do
+        raise "oops"
+      end
+    end
+
+    records = [create_record("Break me")]
+    BrokenProcessor.initialize [largest_seq: 1234]
+    BrokenProcessor.process_records records
+
+    assert content(io[:output]) == "{\"action\":\"checkpoint\",\"checkpoint\":1234}\n"
   end
 
 end
